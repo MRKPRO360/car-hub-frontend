3e006bf25a1426fc272a50224d737e6542d47ce2
Rezaul <mdrezaulkarrim@gmail.com>
2025-07-26 00:13:53 +0600

    added suddle shadow on dark version for mobile app header

--- src/components/CHDashboard/AppHeader.tsx
+++ src/components/CHDashboard/AppHeader.tsx
@@ -148,7 +148,7 @@ const AppHeader: React.FC = () => {
         <div
           className={`${
             isApplicationMenuOpen ? 'flex' : 'hidden'
-          } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
+          } items-center justify-between w-full gap-4 px-5 py-4 lg:flex dark:shadow-gray-800 shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none transition duration-300`}
         >
           <div className="flex items-center gap-2 2xsm:gap-3">
             {/* <!-- Dark Mode Toggler --> */}
