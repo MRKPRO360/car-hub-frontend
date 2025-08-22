d6cc2c647399b01dc3498238924c6ec2cf3be2ac
Rezaul <mdrezaulkarrim@gmail.com>
2025-08-22 23:04:06 +0600

    finished match your budget component animation

--- src/pages/Home/MatchYourBudget.tsx
+++ src/pages/Home/MatchYourBudget.tsx
@@ -1,6 +1,6 @@
 import { useGetAllCarsQuery } from '../../redux/features/admin/carManagement.api';
 import { ICar } from '../../types';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
   Carousel,
   CarouselContent,
@@ -10,34 +10,44 @@ import {
 import Cta from '../shared/Cta';
 import CarouselCarCard from '../shared/CarouselCarCard';
 import CarouselSkeletonCard from '../shared/CarouselSkeletonCard';
+import useCardStagger from '../../hooks/useCardStagger';
 
 const MatchYourBudget = () => {
   const { data: cars, isLoading } = useGetAllCarsQuery(undefined);
-  const [data, setData] = useState<ICar[]>(cars?.data || []);
 
-  useEffect(() => {
-    if (cars?.data?.length && data !== cars.data) {
-      setData(cars?.data);
-    }
-  }, [cars, data]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const headingRef = useRef<HTMLHeadingElement>(null);
+
+  useCardStagger(headingRef, containerRef, cars?.data);
+
   return (
     <section className="dark:from-gray-dark  bg-gradient-to-b via-80% from-white to-[rgba(20,73,230,0.06)] py-12 lg:py-18">
-      <div className="">
-        <h2 className="text-4xl font-bold text-center dark:text-gray-300 text-gray-900 mb-12">
+      <div className="overflow-y-hidden  mb-12">
+        <h2
+          ref={headingRef}
+          className="text-4xl font-bold text-center dark:text-gray-300 text-gray-900 mb-12"
+        >
           Match Your&nbsp;
           <span className="text-blue-600">Budget</span>
         </h2>
 
         <Carousel className="md:w-full ">
-          <CarouselContent className="-ml-1 pb-3 sm:pb-6 ">
-            {isLoading
-              ? Array.from({ length: 4 }).map((_, i) => (
-                  <CarouselSkeletonCard key={i} />
-                ))
-              : data?.map((car: ICar) => (
-                  <CarouselCarCard key={car._id} car={car} />
-                ))}
-          </CarouselContent>
+          {isLoading && (
+            <CarouselContent className="-ml-1 pb-3 sm:pb-6">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <CarouselSkeletonCard key={i} />
+              ))}
+            </CarouselContent>
+          )}
+
+          {!isLoading && (
+            <CarouselContent className="-ml-1 pb-3 sm:pb-6" ref={containerRef}>
+              {cars?.data?.map((car: ICar) => (
+                <CarouselCarCard key={car._id} car={car} />
+              ))}
+            </CarouselContent>
+          )}
+
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
