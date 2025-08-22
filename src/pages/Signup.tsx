0336f333a937fbdb91fb8ea81970ac7c4a7ccede
Rezaul <mdrezaulkarrim@gmail.com>
2025-08-22 10:09:06 +0600

    added google and facebook login on the signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -278,6 +278,8 @@ import { Link, useNavigate } from 'react-router';
 import Cta from './shared/Cta';
 import ImageUpload from './shared/ImageUpload';
 import { FcGoogle } from 'react-icons/fc';
+import GoogleLoginBtn from './shared/GoogleLoginBtn';
+import FBLoginBtn from './shared/FBLoginBtn';
 
 function Signup() {
   const [signup] = useSignupMutation();
@@ -546,7 +548,7 @@ function Signup() {
           <span className="h-px w-full bg-gray-200"></span>
         </div>
         <div className="flex justify-center gap-5 w-full">
-          <button
+          {/* <button
             className="w-full flex items-center justify-center mb-6 md:mb-0 border border-primary text-sm text-gray-500 py-2 px-5 gap-1 rounded-md tracking-wide font-medium cursor-pointer transition ease-in duration-500"
             type="button"
           >
@@ -560,7 +562,10 @@ function Signup() {
           >
             <FaFacebook className="text-2xl text-blue-600" />
             <span>Facebook</span>
-          </button>
+          </button> */}
+
+          <GoogleLoginBtn />
+          <FBLoginBtn />
         </div>
       </div>
     </div>
