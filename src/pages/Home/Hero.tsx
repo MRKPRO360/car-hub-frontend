c9136e1236fd4ea28d7f09d5822cc625a8f1b032
Rezaul <mdrezaulkarrim@gmail.com>
2025-09-06 23:47:18 +0600

    aded headless ui listbox and replaced old select box

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -10,8 +10,16 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { brandOptions } from '../../constant/car';
 import { useGetAllModelsByBrandQuery } from '../../redux/features/admin/carManagement.api';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+  Transition,
+} from '@headlessui/react';
 import { Link, useNavigate } from 'react-router';
+import { ChevronsUpDownIcon } from 'lucide-react';
 
 interface IFormValue {
   brand: string;
@@ -28,12 +36,16 @@ const Hero = () => {
 
   const navigate = useNavigate();
 
-  const { register, watch, handleSubmit } = useForm<IFormValue>({
+  const { control, watch, handleSubmit, setValue } = useForm<IFormValue>({
     defaultValues: { brand: '', model: '' },
   });
 
   const brand = watch('brand');
   const model = watch('model');
+
+  useEffect(() => {
+    setValue('model', '');
+  }, [brand, setValue]);
 
   const {
     data: models,
@@ -125,7 +137,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="text-center overflow-hidden">
+    <section className="text-center overflow-hidden relative">
       <div
         ref={bgRef}
         style={{
@@ -143,9 +155,9 @@ const Hero = () => {
           backgroundRepeat: 'no-repeat',
         }}
         //bg-cover xsm:bg-[length:100%_100%]
-        className="min-h-[60vh] md:min-h-[90vh] lg:py-14 bg-cover xsm:bg-center bg-blend-overlay dark:bg-blend-darken  md:bg-cover grid place-content-center"
+        className="relative min-h-[60vh] md:min-h-[90vh] lg:py-14 bg-cover xsm:bg-center bg-blend-overlay dark:bg-blend-darken  md:bg-cover grid place-content-center"
       >
-        <div className="md:-translate-y-20">
+        <div className="md:-translate-y-20 relative z-10">
           <h1 className="text-3xl xsm:text-4xl md:text-5xl font-bold text-gray-300 mb-4 overflow-hidden">
             <span
               className="text-blue-600 inline-block"
@@ -173,86 +185,180 @@ const Hero = () => {
             className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4 mb-6"
             ref={searchRef}
           >
-            <div className="relative w-60 z-30">
-              <select
-                {...register('brand')}
-                className="px-4 py-3 border-0 rounded-full text-sm w-full bg-white text-gray-800  drop-shadow-xl   focus:ring-2 focus:ring-blue-700 focus:outline-none appearance-none cursor-pointer hover:bg-gray-50 transition-colors duration-200"
-              >
-                <option hidden value="" className="text-gray-400" disabled>
-                  Select make
-                </option>
-                {brandOptions.map((el) => (
-                  <option key={el.label} value={el.value}>
-                    {el.value}
-                  </option>
-                ))}
-              </select>
+            {/* BRAND  */}
 
-              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <svg
-                  className="w-5 h-5 text-blue-600"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+            <Controller
+              name="brand"
+              control={control}
+              render={({ field }) => (
+                <Listbox value={field.value} onChange={field.onChange}>
+                  <div className="relative w-60 z-30">
+                    <ListboxButton className="relative w-full cursor-pointer rounded-full bg-white py-3 px-4 text-left text-sm text-gray-800 shadow-md ring-1 ring-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-600">
+                      <span className="block truncate">
+                        {field.value || 'Select Make'}
+                      </span>
+                      <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                        <ChevronsUpDownIcon className="h-5 w-5 text-blue-600" />
+                      </span>
+                    </ListboxButton>
+
+                    <Transition
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <ListboxOptions className="absolute mt-1 max-h-60 w-full overflow-y-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black/10 focus:outline-none z-50">
+                        {brandOptions.map((el) => (
+                          <ListboxOption
+                            key={el.value}
+                            value={el.value}
+                            className={({ active }) =>
+                              `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? 'bg-blue-100 text-blue-900'
+                                  : 'text-gray-800'
+                              }`
+                            }
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? 'font-medium' : 'font-normal'
+                                  }`}
+                                >
+                                  {el.label}
+                                </span>
+                              </>
+                            )}
+                          </ListboxOption>
+                        ))}
+                      </ListboxOptions>
+                    </Transition>
+                  </div>
+                </Listbox>
+              )}
+            />
+
+            {/* MODEL */}
+            <Controller
+              name="model"
+              control={control}
+              render={({ field }) => (
+                <Listbox
+                  value={field.value}
+                  onChange={field.onChange}
+                  disabled={!brand}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </div>
-            </div>
+                  <div className="relative w-60 z-20">
+                    <ListboxButton
+                      className={`relative w-full cursor-pointer rounded-full py-3 px-4 text-left text-sm shadow-md ring-1 ring-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-600 ${
+                        !brand
+                          ? 'bg-gray-100 text-gray-400 cursor-not-allowed'
+                          : 'bg-white text-gray-800'
+                      }`}
+                    >
+                      <span className="block truncate">
+                        {field.value
+                          ? models?.data?.find((el) => el._id === field.value)
+                              ?.model
+                          : brand
+                          ? 'Select model'
+                          : 'Select make first'}
+                      </span>
+                      <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                        <ChevronsUpDownIcon className="h-5 w-5 text-blue-600" />
+                      </span>
+                    </ListboxButton>
+                    <Transition
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <ListboxOptions className="absolute mt-1 max-h-60 w-full overflow-y-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black/10 focus:outline-none z-40">
+                        {models?.data?.length > 0 ? (
+                          models.data.map((el) => (
+                            <ListboxOption
+                              key={el._id}
+                              value={el._id}
+                              className={({ active }) =>
+                                `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                                  active
+                                    ? 'bg-blue-100 text-blue-900'
+                                    : 'text-gray-800'
+                                }`
+                              }
+                            >
+                              {({ selected }) => (
+                                <span
+                                  className={`block truncate ${
+                                    selected ? 'font-medium' : 'font-normal'
+                                  }`}
+                                >
+                                  {el.model}
+                                </span>
+                              )}
+                            </ListboxOption>
+                          ))
+                        ) : !isLoading && isError ? (
+                          <ListboxOption
+                            className={({ active }) =>
+                              `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? 'bg-blue-100 text-blue-900'
+                                  : 'text-gray-800'
+                              }`
+                            }
+                            value=""
+                          >
+                            {({ selected }) => (
+                              <span
+                                className={`block truncate ${
+                                  selected ? 'font-medium' : 'font-normal'
+                                }`}
+                              >
+                                Something went wrong
+                              </span>
+                            )}
+                          </ListboxOption>
+                        ) : (
+                          <ListboxOption
+                            className={({ active }) =>
+                              `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? 'bg-blue-100 text-blue-900'
+                                  : 'text-gray-800'
+                              }`
+                            }
+                            value=""
+                          >
+                            {({ selected }) => (
+                              <span
+                                className={`block truncate ${
+                                  selected ? 'font-medium' : 'font-normal'
+                                }`}
+                              >
+                                No results available
+                              </span>
+                            )}
+                          </ListboxOption>
+                        )}
+                      </ListboxOptions>
+                    </Transition>
+                  </div>
+                </Listbox>
+              )}
+            />
 
-            <div className="relative w-60 z-30">
-              <select
-                {...register('model')}
-                disabled={!brand}
-                className="px-4 py-3 border-0 rounded-full text-sm w-full bg-white text-gray-800  drop-shadow-xl   focus:ring-2 focus:ring-blue-700 focus:outline-none appearance-none cursor-pointer hover:bg-gray-50 transition-colors duration-200"
-              >
-                {!isLoading && isError && (
-                  <option value="">Something wrong</option>
-                )}
-
-                <option hidden value="" disabled>
-                  {brand ? 'Select model' : 'Select make first'}
-                </option>
-                {models?.data?.length <= 0 && (
-                  <option value="">No results available</option>
-                )}
-                {models?.data?.length > 0 &&
-                  models?.data?.map((el) => (
-                    <option value={el._id} key={el._id}>
-                      {el.model}
-                    </option>
-                  ))}
-              </select>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <svg
-                  className="w-5 h-5 text-blue-600"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </div>
-            </div>
             {model && models && models?.data?.length > 0 ? (
-              <button type="submit">
-                <Cta text="View Car" />
+              <button type="submit" className="z-10">
+                {' '}
+                <Cta text="View Car" />{' '}
               </button>
             ) : (
-              <Link to="/stock">
-                <Cta text="View All Cars" />
+              <Link to="/stock" className="z-10">
+                {' '}
+                <Cta text="View All Cars" />{' '}
               </Link>
             )}
           </form>
